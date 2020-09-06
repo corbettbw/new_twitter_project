1660ec29f6a41e2fb3dd5a776f98c6b2e844a677
Corbett Winningham <Corbett Winningham>
2020-09-05 18:16:08 -0700

    making the algorithm more efficient

--- twitter_client.rb
+++ twitter_client.rb
@@ -1,16 +1,6 @@
-# access token: 316992729-MXAAaoxT7zfyqdo02abNMJEnUfcYq2m00JMeAgEx
-# access token secret: TltLatEppSWN7qMr3Ezj5QAUiOW9GJOtWQmqL3Cy2GfRS
-
-# api key: Y7vDvBVrN28MsuKsO4QTay5bb
-# api secret key: df4yBRTXH6nMQ9VVXJKo5523DpfdYctditjgsAtvm2eVE5MmZQ
-# tweets = client.user_timeline('rubyinside', count: 20 )
-# File.write('tweets.yml', YAML.dump(tweets))
-
-# tweets = YAML.load_file('tweets.yml')
-# tweets.each { |tweet| puts tweet.full_text }
-
 require 'yaml'
 require 'twitter'
+
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = "K4fckJLvqUkNsJNYV0hXAnZtf"
   config.consumer_secret     = "t2rK6AptmZM8nz2JeXSuZo9elfXH8hDZOHXPGZGLBazhwEXmCD"
@@ -18,4 +8,29 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = "JA7QOQXh1K0d4BUZXhGpeNDumTLhqgobPFgla7uZZttJW"
 end
 
-client.update("#twitter_gem")
+primes = [2,3]
+number = 3
+
+client.update("2 is #prime #PrimeNumberBot")
+sleep(100)
+
+client.update("3 is #prime #PrimeNumberBot")
+
+while primes.length > 1
+  number += 2
+  isprime = 0
+  primes.each do |prime|
+    if number % prime == 0
+      isprime += 1
+      break
+    elsif prime >= Math.sqrt(number)
+        break
+    end
+  end
+  if isprime == 0
+    sleep(100)
+    client.update("#{number} is #prime #PrimeNumberBot")
+    primes.push number
+  end
+end
+puts primes
